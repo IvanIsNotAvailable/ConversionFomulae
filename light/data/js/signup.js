1f6e90f3d15cbb72f8c5cb12c7e49b2827993970
IvanIsNotAvailable <111681901+IvanIsNotAvailable@users.noreply.github.com>
2022-12-08 15:21:24 +0630

    Delete light directory

--- light/data/js/signup.js
+++
@@ -1,7 +0,0 @@
-const form = document.querySelector('.signup form');
-console.log(form);
-continueBtn = form.querySelector(".button input");
-
-continueBtn.onclick = () =>{
-    console.log('hello');
-}
